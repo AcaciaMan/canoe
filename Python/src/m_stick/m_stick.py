cab149cd239d1c680861fc621a57f1da9d204f70
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-10-22 01:53:03 +0300

    First prediction with DecisionTreeRegressor

--- Python/src/m_stick/m_stick.py
+++ Python/src/m_stick/m_stick.py
@@ -1,6 +1,18 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
 class M_Stick:
     def __init__(self):
         self.image_path = ""
+        self.pixels = []
+        self.width = 0
+        self.height = 0
+        self.rgbs = {}
+        self.siblings = {}
+        self.df = None
+        self.columns = [f"r{i}" for i in range(171)]
+        self.regressor = DecisionTreeRegressor(random_state=42)
+
     
     def m_stick(self, image_path):
         self.image_path = image_path
@@ -21,13 +33,94 @@ class M_Stick:
                 for aPixel in aPixels:
                     r, g, b = aPixel.split(",")
                     pixels.append((x, y, int(r), int(g), int(b)))
+                    self.rgbs[(x, y)] = (int(r), int(g), int(b))
                     x += 1
                 y += 1
                 width = x
                 height = y
                 x = 0
 
+        self.pixels = pixels
+        self.width = width
+        self.height = height
+
 
         print(f"Image size: {width}x{height}")
 
         return pixels, width, height
+    
+    def get_siblings(self):
+        # get the siblings of each pixel
+        for i in range(len(self.pixels)):
+            x, y, r, g, b = self.pixels[i]
+            siblings = [(r, g, b)]
+            for k in range(1, 8):
+                for j in range(1, 9):
+                    if j == 1:
+                        x1, y1 = x-k, y-k
+                    if j == 2:
+                        x1, y1 = x, y-k
+                    if j == 3:
+                        x1, y1 = x+k, y-k
+                    if j == 4:
+                        x1, y1 = x-k, y
+                    if j == 5:
+                        x1, y1 = x+k, y
+                    if j == 6:
+                        x1, y1 = x-k, y+k
+                    if j == 7:
+                        x1, y1 = x, y+k
+                    if j == 8:
+                        x1, y1 = x+k, y+k
+                    try:
+                        siblings.append(self.rgbs[(x1, y1)])
+                    except KeyError:
+                        siblings.append((1, 1, 1))
+
+            self.siblings[(x, y)] = siblings
+
+    def get_df(self):
+        # create a DataFrame for some pixels
+        # the DataFrame will have columns r0, g0, b0, r1, g1, b1, ..., r56, g56, b56 for some pixel (x, y) from the self.siblings
+        pixels = []
+        target = []
+        bad = [(80,63,56),(109,127,125),(172,141,138),(153,174,171),(153,173,171),(154,174,172),(155,176,173),(158,177,175),(154,173,171),(157,175,173),(168,187,185),(161,179,177)  ]
+        good = [(152,156,146),(21,37,36),(0,0,0),(0,5,6),(2,9,10),(1,5,6),(0,3,4),(0,0,0),(0,0,0),(92,48,57),(101,8,27),(100,7,26),(121,0,23),(123,0,25),(128,0,26),(129,0,28),(108,13,34),(130,36,56)]
+        for (x, y), siblings in self.siblings.items():
+            r, g, b = self.rgbs[(x, y)]
+            if ((r, g, b) in bad):
+                target.append(0)  
+                pixels.append([c for rgb in siblings for c in rgb])
+            elif ((r, g, b) in good):
+                target.append(1)  
+                pixels.append([c for rgb in siblings for c in rgb])    
+
+        df = pd.DataFrame(pixels, columns=self.columns)
+        df["target"] = target  
+
+        self.df = df
+
+    def train_model_regressor(self):
+        # train a regressor model
+
+        X = self.df[self.columns]
+        y = self.df["target"]
+
+        self.regressor = DecisionTreeRegressor(random_state=42)
+        self.regressor.fit(X, y)
+
+
+    def predict(self):
+        # predict the target for the siblings of each pixel
+        pixels = []
+        for (x, y), siblings in self.siblings.items():
+            X = pd.DataFrame([c for rgb in siblings for c in rgb]).T
+            target = self.regressor.predict(X)[0]
+            if target < 0.5:
+                pixels.append((x, y, 1, 1, 1))
+            else:
+                r, g, b = self.rgbs[(x, y)]
+                pixels.append((x, y, r, g, b))
+        return pixels        
+
+
--- Python/src/routes.py
+++ Python/src/routes.py
@@ -12,11 +12,16 @@ def stick_image():
         
         mStick = M_Stick()
         pixels, width, height = mStick.m_stick(image_path)
+
+        mStick.get_siblings()
+        mStick.get_df()
+        mStick.train_model_regressor()
+        pixels = mStick.predict()
+
         
         
         # Convert the array to dictionary with strings
         pixel_dict_str_keys = {f"{x},{y}": (r, g, b) for (x, y, r, g, b) in pixels}
-        print(pixel_dict_str_keys)
 
         return jsonify({"message": "Image processed successfully", "width": width, "height": height, "pixels": pixel_dict_str_keys})
     return jsonify({"message": "No file uploaded"}), 400
