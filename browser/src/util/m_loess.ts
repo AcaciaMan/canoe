a2ad78317357f66bf43088ff91352d1b1033d0e0
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-10-27 13:39:06 +0200

    introduced slice

--- browser/src/util/m_loess.ts
+++ browser/src/util/m_loess.ts
@@ -10,30 +10,60 @@ export class Loess {
 
   mPreCalcLoess: M_PreCalcLoess = M_PreCalcLoess.getInstance();
 
+
   smooth(y: Uint8Array): Int16Array {
     const n = y.length;
     const smoothed = new Int16Array(n);
 
+    let totalFilter = 0;
+    let totalEndedWeights = 0;
+    let totalEndedSumWeightedY = 0;
+    let totalEndedMeanY = 0;
+    let totalEndedBeta = 0;
+    let totalEndedAlpha = 0;
+
     for (let i = 0; i < n; i++) {
-      const around = y.filter((_, idx) => Math.abs(i - idx) <= this.mPreCalcLoess.around);
+      const startedFilter = performance.now();
+      const around = y.slice(Math.max(0, i - this.mPreCalcLoess.around), Math.min(n, i + this.mPreCalcLoess.around));
+      const endedFilter = performance.now();
+      totalFilter += endedFilter - startedFilter;
       if (around.length < this.mPreCalcLoess.around*2) {
         smoothed[i] = y[i];
         continue;
       }
       const weights = this.mPreCalcLoess.getWeights();
       
+      const startedWeights = performance.now();
       const weightedY = [...around].map((yi, idx) => yi * weights[idx]);
+      const endedWeights = performance.now();
+      totalEndedWeights += endedWeights - startedWeights;
       const sumWeightedY = ss.sum(weightedY);
+      const endedSumWeightedY = performance.now();
+      totalEndedSumWeightedY += endedSumWeightedY - endedWeights;
 
       const meanY = sumWeightedY / this.mPreCalcLoess.getSumWeights();
+      const endedMeanY = performance.now();
+      totalEndedMeanY += endedMeanY - endedSumWeightedY;
+
 
       const beta =
         (sumWeightedY - meanY * this.mPreCalcLoess.getSumWeights()) /
         (this.mPreCalcLoess.getSumWeightedX2() -
           this.mPreCalcLoess.getMeanX() * this.mPreCalcLoess.getSumWeightedX());
+      const endedBeta = performance.now();
+      totalEndedBeta += endedBeta - endedMeanY;    
       const alpha = meanY - beta * this.mPreCalcLoess.getMeanX();
+      const endedAlpha = performance.now();
+      totalEndedAlpha += endedAlpha - endedBeta;
       smoothed[i] = Math.floor(alpha + beta * this.mPreCalcLoess.around);
     }
+
+    console.log(`totalFilter: ${totalFilter}`);
+    console.log(`totalEndedWeights: ${totalEndedWeights}`);
+    console.log(`totalEndedSumWeightedY: ${totalEndedSumWeightedY}`);
+    console.log(`totalEndedMeanY: ${totalEndedMeanY}`);
+    console.log(`totalEndedBeta: ${totalEndedBeta}`);
+    console.log(`totalEndedAlpha: ${totalEndedAlpha}`);
 
     return smoothed;
   }
