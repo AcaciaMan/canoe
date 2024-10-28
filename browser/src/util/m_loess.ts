5b3d136d72f9ceabdf676752e5cd27b5e25eee9c
AcaciaMan <11488694+AcaciaMan@users.noreply.github.com>
2024-10-28 16:08:45 +0200

    Speed up loess smooth calculations

--- browser/src/util/m_loess.ts
+++ browser/src/util/m_loess.ts
@@ -8,30 +8,41 @@ export class Loess {
   }
 
   mPreCalcLoess: M_PreCalcLoess = M_PreCalcLoess.getInstance();
-
+  weightedY = new Float32Array(this.mPreCalcLoess.around[0]*2).fill(0);
+  sumWeightedY = new Float32Array(1);
+  meanY = new Float32Array(1);
+  beta = new Float32Array(1);
+  alpha = new Float32Array(1);
 
   smooth(y: Uint8Array): Int16Array {
     const n = y.length;
     const smoothed = new Int16Array(n);
     for (let i = 0; i < n; i++) {
-    const around = y.slice(Math.max(0, i - this.mPreCalcLoess.around), Math.min(n, i + this.mPreCalcLoess.around));
-    if (around.length < this.mPreCalcLoess.around * 2) {
-      smoothed[i] = y[i];
-      continue;
-    }
-    const weights = this.mPreCalcLoess.getWeights();
-    const weightedY = [...around].map((yi, idx) => yi * weights[idx]);
-    const sumWeightedY = weightedY.reduce((acc, val) => acc + val, 0);
-    const meanY = sumWeightedY / this.mPreCalcLoess.getSumWeights();
-    const beta =
-      (sumWeightedY - meanY * this.mPreCalcLoess.getSumWeights()) /
-      (this.mPreCalcLoess.getSumWeightedX2() -
-      this.mPreCalcLoess.getMeanX() * this.mPreCalcLoess.getSumWeightedX());
-    const alpha = meanY - beta * this.mPreCalcLoess.getMeanX();
-    smoothed[i] = Math.floor(alpha + beta * this.mPreCalcLoess.around);
+      const around = y.slice(
+        Math.max(0, i - this.mPreCalcLoess.around[0]),
+        Math.min(n, i + this.mPreCalcLoess.around[0])
+      );
+      if (around.length < this.mPreCalcLoess.around[0] * 2) {
+        smoothed[i] = y[i];
+        continue;
+      }
+      const weights = this.mPreCalcLoess.getWeights();
+      this.weightedY = this.weightedY.map((_, idx) => around[idx] * weights[idx]); 
+      this.sumWeightedY[0] = this.weightedY.reduce((acc, val) => acc + val, 0);
+      this.meanY[0] =
+        this.sumWeightedY[0] / this.mPreCalcLoess.getSumWeights()[0];
+      this.beta[0] =
+        (this.sumWeightedY[0] -
+          this.meanY[0] * this.mPreCalcLoess.getSumWeights()[0]) /
+        (this.mPreCalcLoess.getSumWeightedX2()[0] -
+          this.mPreCalcLoess.getMeanX()[0] *
+            this.mPreCalcLoess.getSumWeightedX()[0]);
+      this.alpha[0] =
+        this.meanY[0] - this.beta[0] * this.mPreCalcLoess.getMeanX()[0];
+      smoothed[i] = Math.floor(
+        this.alpha[0] + this.beta[0] * this.mPreCalcLoess.around[0]
+      );
     }
     return smoothed;
-
   }
-
 }
--- browser/src/util/m_pre_calc_loess.ts
+++ browser/src/util/m_pre_calc_loess.ts
@@ -1,4 +1,3 @@
-import * as ss from "simple-statistics";
 
 export class M_PreCalcLoess {
   // singleton
@@ -11,17 +10,15 @@ export class M_PreCalcLoess {
     return M_PreCalcLoess.instance;
   }
 
-  weights: number[] | undefined;
-  maxDistance = 50;
-  around = 30;
-  sumWeights: number | undefined;
-  weightedX: number[] | undefined;
-  weightedX2: number[] | undefined;
-  sumWeightedX: number | undefined;
-    sumWeightedX2: number | undefined;
-    meanX: number | undefined;
-randomNumbers: number[] = Array.from({ length: 11 }, () => Math.random());
-ith = 0;
+  weights: Float32Array | undefined;
+  maxDistance = new Uint8Array(1).fill(50);
+  around = new Uint8Array(1).fill(30);
+  sumWeights: Float32Array | undefined;
+  weightedX: Float32Array | undefined;
+  weightedX2: Float32Array | undefined;
+  sumWeightedX: Float32Array | undefined;
+  sumWeightedX2: Float32Array | undefined;
+  meanX: Float32Array | undefined;
 
   getWeights() {
     if (this.weights === undefined) {
@@ -32,68 +29,66 @@ ith = 0;
 
   getSumWeights() {
     if (this.sumWeights === undefined) {
-      this.sumWeights = ss.sum(this.getWeights());
+      this.sumWeights = new Float32Array(1);
+      this.sumWeights[0] = this.getWeights().reduce((acc, val) => acc + val, 0);
     }
     return this.sumWeights;
   }
 
   // calculate weights for loess smoothing
   calculateWeights() {
-    const n = this.around * 2+1;
-    const weights = new Array(n);
+    const n = this.around[0] * 2 + 1;
+    const weights = new Float32Array(n);
     const startIdx = 0;
     const endIdx = n;
     for (let i = startIdx; i < endIdx; i++) {
-      const distance = Math.abs(i - this.around) / this.maxDistance;
+      const distance = Math.abs(i - this.around[0]) / this.maxDistance[0];
       weights[i] = Math.pow(1 - Math.pow(distance, 3), 3);
     }
     return weights;
   }
 
-    getWeightedX() {
-        if (this.weightedX === undefined) {
-        this.weightedX = this.getWeights().map((xi, idx) => xi * idx);
-        }
-        return this.weightedX;
+  getWeightedX() {
+    if (this.weightedX === undefined) {
+      this.weightedX = this.getWeights().map((xi, idx) => xi * idx);
     }
+    return this.weightedX;
+  }
 
-    getWeightedX2() {
-        if (this.weightedX2 === undefined) {
-        this.weightedX2 = this.getWeights().map((xi, idx) => xi * idx * idx);
-        }
-        return this.weightedX2;
+  getWeightedX2() {
+    if (this.weightedX2 === undefined) {
+      this.weightedX2 = this.getWeights().map((xi, idx) => xi * idx * idx);
     }
+    return this.weightedX2;
+  }
 
-    getSumWeightedX() {
-        if (this.sumWeightedX === undefined) {
-        this.sumWeightedX = ss.sum(this.getWeightedX());
-        }
-        return this.sumWeightedX;
+  getSumWeightedX() {
+    if (this.sumWeightedX === undefined) {
+      this.sumWeightedX = new Float32Array(1);
+      this.sumWeightedX[0] = this.getWeightedX().reduce(
+        (acc, val) => acc + val,
+        0
+      );
     }
+    return this.sumWeightedX;
+  }
 
-    getSumWeightedX2() {
-        if (this.sumWeightedX2 === undefined) {
-        this.sumWeightedX2 = ss.sum(this.getWeightedX2());
-        }
-        return this.sumWeightedX2;
+  getSumWeightedX2() {
+    if (this.sumWeightedX2 === undefined) {
+      this.sumWeightedX2 = new Float32Array(1);
+      this.sumWeightedX2[0] = this.getWeightedX2().reduce(
+        (acc, val) => acc + val,
+        0
+      );
     }
+    return this.sumWeightedX2;
+  }
 
-    getMeanX() {
-        if (this.meanX === undefined) {
-        this.meanX = this.getSumWeightedX() / this.getSumWeights();
-        }
-        return this.meanX;
+  getMeanX() {
+    if (this.meanX === undefined) {
+      this.meanX = new Float32Array(1);
+      this.meanX[0] = this.getSumWeightedX()[0] / this.getSumWeights()[0];
     }
-
-    getRandomMultiplication(n: number) {
-        this.ith++;
-        if (this.ith === 11) {
-        this.ith = 0;
-        }
-        return this.randomNumbers[this.ith] * n;
-    }
-
-
-
-
+    return this.meanX;
+  }
 }
